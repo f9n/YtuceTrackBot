73bab4db565feef018d11aa99cc483852b1ef55f
pleycpl <h4rvey@protonmail.com>
2017-05-27 13:39:40 +0000

    if doesnt exist .env file, create .env file

--- init.sh
+++ init.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+read -p 'There is .env file in bot?(y/n) : ' status
+if [[ "$status" = "n" ]]; then
+  read -p "Telegram Token:" token
+  read -p "MongoDbUri:" mongodburi
+  echo "MongoDbUri='$mongodburi'" > bot/.env
+  echo "TelegramToken='$token'" >> bot/.env
+fi
 cd crawler;
 virtualenv venv
 source venv/bin/activate
