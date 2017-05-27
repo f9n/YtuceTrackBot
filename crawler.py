c987c638b59b7a2eef0d2495a896ead309dad4e1
pleycpl <h4rvey@protonmail.com>
2017-05-27 11:11:15 +0000

    creating hash id

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import re
+import hashlib
 url = 'https://ytuce.maliayas.com/'
 r  = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
@@ -9,7 +10,8 @@ for item in soup.find_all('div', 'item'):
     author = item.find('div', 'text_author').find('a')
     title = item.find('div', 'text_title').find('a')
     content = item.find('div', 'text_content').get_text()
-    time = re.search('\d{2}\.\d{2}\.\d{4} \d{2}:\d{2}', item.find('span', 'date')['title'])
+    id = hashlib.md5(content.encode('utf-8')).hexdigest()
+    time = re.search('(\d{2}\.\d{2}\.\d{4}) (\d{2}:\d{2})', item.find('span', 'date')['title'])
     data = {
         'authorName': author.get_text(),
         'authorLink': author['href'],
@@ -17,6 +19,8 @@ for item in soup.find_all('div', 'item'):
         'titleLink': title['href'],
         'content': content,
         'id': id,
-        'date': time.group(0)
+        'date': time.group(1),
+        'clock': time.group(2),
+        'status': 'new'
     }
     print(data)
